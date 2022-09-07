1fa61122c80898259b8937d8741ce59a8d56e151
Su Weijie <weijie.su@shopee.com>
2022-09-07 16:59:36 +0800

    feat(http-client): macos ok

--- packages/http-client/src-tauri/src/app_search/macos.rs
+++ packages/http-client/src-tauri/src/app_search/macos.rs
@@ -4,24 +4,50 @@ use walkdir::{DirEntry, WalkDir};
 
 use super::{AppDataType, AppPath};
 
-static PATH: &'static str = "/Applications";
+static PATH: &str = "/Applications";
 pub fn get_app_data() -> Option<AppDataType> {
     let mut map = HashMap::new();
     let path = PathBuf::from(PATH);
-    search_by_path(&path);
+    search_by_path(&path, &mut map);
 
     Some(map)
 }
 
-fn search_by_path(path: &PathBuf) -> Vec<AppPath> {
-    WalkDir::new(path)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(filter_app)
-        .for_each(|x| {
-            println!("{}", x.path().display());
-        });
-    todo!()
+fn search_by_path(path: &PathBuf, map: &mut HashMap<PathBuf, AppPath>) {
+    let mut it = WalkDir::new(path).into_iter();
+    loop {
+        let entry = match it.next() {
+            None => break,
+            Some(Err(_)) => continue,
+            Some(Ok(entry)) => entry,
+        };
+        if filter_app(&entry) {
+            it.skip_current_dir();
+            let app = map_app(&entry);
+            if let Some(app) = app {
+                map.insert(app.path.clone(), app);
+            }
+        }
+    }
+}
+
+fn map_app(entry: &DirEntry) -> Option<AppPath> {
+    let path = entry.path().to_path_buf();
+    let name = path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string()
+        .replace(".app", "");
+    let desc = None;
+    let icon = None;
+    Some(AppPath {
+        desc,
+        icon,
+        path,
+        name,
+    })
 }
 
 fn filter_app(dir: &DirEntry) -> bool {
