c1e35f7a0efee7f1e14da75295c45d49547e6c1f
Su Weijie <weijie.su@shopee.com>
2022-09-09 16:50:29 +0800

    feat(http-client): macos input size

--- packages/http-client/src/pages/Home.tsx
+++ packages/http-client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Box, List, ListItemButton, ListItemText, TextField } from '@mui/materia
 import { invoke } from '@tauri-apps/api';
 import { appWindow, LogicalSize } from '@tauri-apps/api/window';
 import { open } from '@tauri-apps/api/shell';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useKey } from 'react-use';
 
 interface AppPath {
@@ -15,6 +15,7 @@ interface AppPath {
 export default function Home() {
   const [appPath, setAppPath] = useState([] as AppPath[]);
   const [selectIndex, setSelectIndex] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
   const add = () => {
     if (selectIndex < appPath.length - 1) {
       setSelectIndex((value) => value + 1);
@@ -39,20 +40,26 @@ export default function Home() {
     [selectIndex, appPath],
   );
   useEffect(() => {
+    if (ref.current && ref.current) {
+      ref.current?.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    }
+  }, [ref.current]);
+  useEffect(() => {
     (async () => {
       if (appPath.length !== 0) {
         appWindow.setSize(new LogicalSize(800, 580));
         setSelectIndex(0);
       } else {
-        appWindow.setSize(new LogicalSize(800, 36));
+        appWindow.setSize(new LogicalSize(800, 56));
       }
     })();
   }, [appPath]);
   return (
-    <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+    <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#ffffff00' }}>
       <TextField
         placeholder="搜索"
         fullWidth
+        inputProps={{ spellcheck: 'false' }}
         onChange={async (e) => {
           const data: AppPath[] = await invoke('app_search', { path: e.target.value });
           setAppPath(data);
@@ -66,7 +73,7 @@ export default function Home() {
       />
       <List sx={{ flex: '1 1 0', padding: 0, overflowY: 'auto' }}>
         {appPath.map(({ name, desc, path }, index) => (
-          <ListItemButton key={path} selected={index === selectIndex}>
+          <ListItemButton ref={index === selectIndex ? ref : undefined} key={path} selected={index === selectIndex}>
             <ListItemText primary={name} secondary={desc ?? path} />
           </ListItemButton>
         ))}
